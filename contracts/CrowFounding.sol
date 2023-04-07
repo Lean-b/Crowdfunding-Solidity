3cf13acbaf1052531b83e01250937846359e4aef
LeanBorquez <leandroborqueza@gmail.com>
2023-04-07 17:50:41 -0300

    smart contract

--- contracts/CrowFounding.sol
+++ contracts/CrowFounding.sol
@@ -1,6 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+/*
+@title CrowFounding (Donations)
+@author Leandro
+@notice This smart contract is a basic crowfounding  that recover ether (donations)
+@dev This smart contrac is build only for me learning.I am not an expert
+*/
 
 
 contract CrowFounding{
@@ -23,28 +29,35 @@ contract CrowFounding{
       _;
     }
 
+    // @notice Set the fundraising goal
+    // @param _goalCont The new fundraising goal
     function goal(uint256 _goalCont) external onlyOwner {
       require(_goalCont >=goalContribution);
       goalContribution = _goalCont;
     }
 
+    // @notice Withdraw funds after reaching the fundraising goal
     function withdraw() external onlyOwner{
         require(fundContribution >= goalContribution);
         payable(owner).transfer(address(this).balance);
         fundContribution = 0;
     }
 
+    // @notice Get the current balance of the contract
+    // @return The current balance of the contract
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-
+    // @notice Accept donations for the fundraising campaign
+    // @param _amount The amount of the donation
     function donation(uint256 _amount)public payable {
       require(block.timestamp < deadline, "Period has ended");
       require(msg.value == _amount, "Sent amount");
       fundContribution += _amount;
     }
-
+    
+    // @notice Receive ETH donations
     receive() external payable{
       donation(msg.value);
     }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config({ path: ".env" });
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "goerli",
+  networks: {
+     goerli: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+     }
+  },
 };
